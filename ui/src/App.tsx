109ce0ac535268ab004aef5bbc6a116efe0f7acb
Yanislav Ivanov <yanislav.2k2@gmail.com>
2025-05-14 10:19:10 +0300

    navigation implemented

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,34 +1,98 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Menu, Layout, Space, Image } from 'antd'
+import {
+  AreaChartOutlined,
+  CalendarOutlined,
+  FormOutlined,
+  HomeOutlined,
+  LoginOutlined,
+  MessageOutlined,
+  SettingOutlined
+} from '@ant-design/icons'
+
+const { Sider, Content } = Layout
+
+
+const items = [
+  {
+    key: '1',
+    icon: <HomeOutlined />,
+    label: 'Home'
+  },
+  {
+    key: '2',
+    icon: <CalendarOutlined />,
+    label: 'Calendar'
+  },
+  {
+    key: '3',
+    icon: <FormOutlined />,
+    label: 'Tasks'
+  },
+  {
+    key: '4',
+    icon: <AreaChartOutlined />,
+    label: 'Statistics'
+  },
+  {
+    key: '5',
+    icon: <LoginOutlined />,
+    label: 'Login'
+  },
+  {
+    key: '6',
+    icon: <SettingOutlined />,
+    label: 'Settings'
+  },
+  {
+    key: '7',
+    icon: <MessageOutlined />,
+    label: 'Feedback'
+  }
+]
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [selectedKey, setSelectedKey] = useState('1')
+  const [collapsed, setCollapsed] = useState(false)
+
+  const renderContent = (key: string) => {
+    switch (key) {
+      case '1':
+        return <div>Content for Navigation One</div>
+      case '2':
+        return <div>Content for Navigation Two</div>
+      case '13':
+        return <div>Content for Option 13</div>
+      case '14':
+        return <div>Content for Option 14</div>
+      default:
+        return <div>Select a menu item</div>
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout style={{ minHeight: '100vh', minWidth: '100vw' }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={setCollapsed}
+        width={260}
+      >
+        <Space >
+          <Image src='../public/logo.png' width='80px' preview={false} />
+        </Space>
+        <Menu
+          mode="inline"
+          selectedKeys={[selectedKey]}
+          onClick={(e) => { setSelectedKey(e.key) }}
+          items={items}
+        />
+      </Sider>
+      <Content style={{ padding: '24px', backgroundColor: '#1e1e1e', color: 'white', flex: 1 }}>
+        {renderContent(selectedKey)}
+      </Content>
+    </Layout>
   )
 }
 
